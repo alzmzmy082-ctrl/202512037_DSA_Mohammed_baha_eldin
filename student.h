8d7ed5f11f48148c42b4c61fc98b67ccfc4b26be
alzmzmy082-ctrl <alzmzmy082@gmail.com>
2026-05-25 23:16:49 +0200

    Add files via upload

---
+++ student.h
@@ -0,0 +1,22 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Student {
+    int id;
+    string name;
+    int age;
+    string course;
+};
+
+void addStudent();
+void displayStudents();
+void searchStudent();
+void updateStudent();
+void deleteStudent();
+
+#endif
